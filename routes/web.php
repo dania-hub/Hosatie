c0b6d8c930ab7d6cd8c347643d6352e8afe4ab98
aya-almokhtar03 <aayoota2@gmail.com>
2025-12-01 00:30:13 +0200

    .

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,60 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-Route::get('/', function () {
-    return Inertia::render('DataEntry/patientList');
+ Route::get('/', function () {
+   return Inertia::render('DepartmentMa/patientListd');
 });
 
-Route::get('/OperationLog', function () {
-    return Inertia::render('DataEntry/operationLog');
+ Route::get('/OperationLog', function () {
+    return Inertia::render('DepartmentMa/operationLogd');
 });
 
 Route::get('/Statistics', function () {
-    return Inertia::render('DataEntry/statistics');
+    return Inertia::render('DepartmentMa/statisticsd');
+});
+
+Route::get('/SuRequests', function () {
+    return Inertia::render('DepartmentMa/SuRequests');
 });
 
 Route::get('/profile', function () {
     return Inertia::render('profile');
 });
+
+
+
+
+
+// Route::get('/', function () {
+//     return Inertia::render('DataEntry/patientList');
+// });
+
+// Route::get('/OperationLog', function () {
+//     return Inertia::render('DataEntry/operationLog');
+// });
+
+// Route::get('/Statistics', function () {
+//     return Inertia::render('DataEntry/statistics');
+// });
+
+// Route::get('/profile', function () {
+//     return Inertia::render('profile');
+// });
+
+
+
+//  Route::get('/', function () {
+//     return Inertia::render('Pharmacist/medicationsList');
+//  });
+
+// // Route::get('/OperationLog', function () {
+// //      return Inertia::render('Doctor/operationLogd');
+// //  });
+
+// //  Route::get('/Statistics', function () {
+// //      return Inertia::render('Doctor/statisticsd');
+// //  });
+
+//  Route::get('/profile', function () {
+//      return Inertia::render('profile');
+//  });
